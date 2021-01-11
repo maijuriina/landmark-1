772729a80b80294838bf6eb4b2d0d65ce6020624
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-01-11 14:02:51 +0200

    Recreated another project to fix the caching bug. Imported images from tutorial and json-file.

---
+++ landmarks/Shared/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  Shared
+//
+//  Created by Maijuriina Mustonen on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
