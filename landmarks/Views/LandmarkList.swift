bdc74bd0ec0c5cb4ea9835bcc67790b5cb1ff341
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-01-18 09:49:32 +0200

    Added iPad to previews list, and started working on isFavorite

--- landmarks/Views/LandmarkList.swift
+++ landmarks/Views/LandmarkList.swift
@@ -23,7 +23,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+        ForEach(["iPhone SE", "iPhone XS Max", "iPad Pro (12.9-inch) (2nd generation)"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
